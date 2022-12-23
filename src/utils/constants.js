16f0cc7762b1609b9130cbbd6da10786d682a4cc
rgdmprmd <ranggadpermadi@gmail.com>
2022-12-23 10:36:46 +0800

    Update API endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_URL = "https://bdcode-quran.herokuapp.com/";
+export const API_URL = "https://quran-api-seven.vercel.app/";
+// export const API_URL = "https://api.quran.gading.dev/surah/1/";
+// export const API_URL = "https://bdcode-quran.herokuapp.com/";`
 // export const API_URL = "https://api.quran.sutanlab.id/";
 // export const API_URL = "https://api.npoint.io/99c279bb173a6e28359c/";
 // export const API_URL2 = "https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3";
